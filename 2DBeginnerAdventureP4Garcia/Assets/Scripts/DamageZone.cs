3d7f7518f469d2262552dba5e55bc6c03d11b7b0
voidddd7 <726888@student.dusd.net>
2024-11-05 11:33:31 -0800

    i added half the damage script
    i codded half of the damage script for my project.

---
+++ 2DBeginnerAdventureP4Garcia/Assets/Scripts/DamageZone.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Android.LowLevel;
+
+public class DamageZone : MonoBehaviour
+{
+private void OnTriggerStay2D(Collider2D other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();   
+
+        if(controller != null )
+        {
+            controller.ChangeHealth(-1);
+        }
+    }
+}
--- 2DBeginnerAdventureP4Garcia/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP4Garcia/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
+
     public int maxHealth = 5;
+    public float timeInvincible = 2;
    public int health { get { return currentHealth; } }
     int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -26,6 +31,15 @@ public class PlayerController : MonoBehaviour
          horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        if(isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if(invincibleTimer < 0 )
+            {
+                isInvincible = false;
+            }
+        }
+
        
     }
     void FixedUpdate()
@@ -38,6 +52,15 @@ public class PlayerController : MonoBehaviour
     }
     public void ChangeHealth(int amount)
     {
+        if(amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
