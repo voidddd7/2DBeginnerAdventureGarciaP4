716c382b940ba9072f58d3938db903d9152b3fe7
voidddd7 <726888@student.dusd.net>
2024-11-22 11:47:20 -0800

    i finished my UI
    i finished my Ui and submitted

--- 2DBeginnerAdventureP4Garcia/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP4Garcia/Assets/Scripts/PlayerController.cs
@@ -89,7 +89,7 @@ public class PlayerController : MonoBehaviour
             invincibleTimer = timeInvincible;
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHealthBar.instance.SetValue(currentHealth/(float)maxHealth);
     }
    
     void Launch()
